a6b515f503241555cb7c0e7ca9d499741a42202e
UsamMDSI <usam.adhikari@student.uts.edu.au>
2026-05-26 01:02:58 +1000

    Update __init__.py

--- classes/__init__.py
+++ classes/__init__.py
@@ -3,3 +3,15 @@ from .fetcher import NewsFetcher
 from .scraper import Scraper
 from .processor import DataProcessor
 from .visualizer import Visualizer
+from .app import NewsApp
+
+__all__ = [
+    "NewsArticle",
+    "APIArticle",
+    "ScrapedArticle",
+    "NewsFetcher",
+    "Scraper",
+    "DataProcessor",
+    "Visualizer",
+    "NewsApp",
+]
